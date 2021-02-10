9e803fccfc2de3c6e06adbcdd77376e3f2a652d9
Rodrigo Monastero <romonastero@gmail.com>
2021-02-10 19:59:59 -0300

    Alterando comentarios

--- meuPrimeiroProgramaJava/principal.java
+++ meuPrimeiroProgramaJava/principal.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 public class principal {
 	public static void main(String[] args) {
 		
+		//Saldaçao e pergunta nome
 		System.out.println("Olá amigo!\nQual é o seu nome?");
 		
+		// Ler o nome
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
+		//Saldação especifica
 		System.out.printf("Olá %s!\n", nome);
 		in.close();
 
